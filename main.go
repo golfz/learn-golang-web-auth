68d6b4d21303df1bd8b88daed8defb619cd56ef6
Surattikorn <surattikorn@gmail.com>
2020-09-23 10:33:49 +0700

    init http service

--- main.go
+++ main.go
@@ -1,37 +1,46 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
+import "net/http"
 
 func main() {
-	type Person struct {
-		Firstname string
-	}
+	//type Person struct {
+	//	Firstname string
+	//}
+	//
+	//p1 := Person{
+	//	Firstname: "GolFz",
+	//}
+	//
+	//p2 := Person{
+	//	Firstname: "Tun",
+	//}
+	//
+	//xp := []Person{p1, p2}
+	//
+	//bs, err := json.Marshal(xp)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Println("struct to json:", string(bs))
+	//
+	//xp2 := []Person{}
+	//err = json.Unmarshal(bs, &xp2)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Println("json to struct:", xp2)
 
-	p1 := Person{
-		Firstname: "GolFz",
-	}
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", bar)
+	http.ListenAndServe(":9090", nil)
+}
 
-	p2 := Person{
-		Firstname: "Tun",
-	}
+func foo(w http.ResponseWriter, r *http.Request) {
 
-	xp := []Person{p1, p2}
+}
 
-	bs, err := json.Marshal(xp)
-	if err != nil {
-		panic(err)
-	}
+func bar(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Println("struct to json:", string(bs))
-
-	xp2 := []Person{}
-	err = json.Unmarshal(bs, &xp2)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("json to struct:", xp2)
 }
